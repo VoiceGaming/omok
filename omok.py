0b4fc9904d1a5985792580eafd3c1bf16b02f324
unknown <rohtj_312@dgu.ac.kr>
2025-03-24 04:24:21 +0900

    first commit

---
+++ omok.py
@@ -0,0 +1,209 @@
+import tkinter as tk
+from tkinter import messagebox
+from speech_recognizer_model import SpeechRecognizer
+
+VOICE_INPUT = 1
+VOICE_CHECK = 2
+GAME_CHECK = 3
+GAME_OVER = 4
+EXIT = 5
+LOADING = 6
+
+ERROR = 0
+NO = 1
+YES = 2
+
+class Omok:
+    def __init__(self, root, model):
+        self.root = root
+        self.root.title("Omok Game")
+
+        self.board_size = 14
+        self.cell_size = 40
+        self.canvas_size = self.board_size * self.cell_size
+
+        self.label = tk.Label(root, text="LOADING...", font=("Arial", 16, "bold"))
+        self.label.pack()
+        
+        self.state_label = tk.Label(root, text=" ", font=("Arial", 12))
+        self.state_label.pack()
+        
+        self.canvas = tk.Canvas(root, width=self.canvas_size, height=self.canvas_size, bg="burlywood")
+        self.canvas.pack()
+        
+
+        self.draw_board()
+        self.board = [[None] * self.board_size for _ in range(self.board_size)]
+        self.current_player = "black"
+
+        self.model = model
+        
+        self.state = LOADING
+        
+        self.row = None
+        self.col = None
+        
+        self.flg = True
+        
+        self.root.after(8000, self.state_machine)
+        
+
+    def draw_board(self):
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                x1, y1 = j * self.cell_size, i * self.cell_size
+                x2, y2 = x1 + self.cell_size, y1 + self.cell_size
+                self.canvas.create_rectangle(x1, y1, x2, y2, outline="black")
+                label = f"{chr(65+i)}{j+1}"
+                self.canvas.create_text((x1 + x2) // 2, (y1 + y2) // 2, text=label, 
+                                        font=("Arial", 10, "bold"), fill="gray")
+                
+    
+    def check_winner(self, row, col):
+        row = row - 1
+        col = col - 1
+        
+        directions = [(1, 0), (0, 1), (1, 1), (1, -1)]
+        player = self.board[row][col]
+        
+        for dr, dc in directions:
+            count = 1
+            for step in range(1, 5):
+                r, c = row + dr * step, col + dc * step
+                if 0 <= r < self.board_size and 0 <= c < self.board_size and self.board[r][c] == player:
+                    count += 1
+                else:
+                    break
+            for step in range(1, 5):
+                r, c = row - dr * step, col - dc * step
+                if 0 <= r < self.board_size and 0 <= c < self.board_size and self.board[r][c] == player:
+                    count += 1
+                else:
+                    break
+            if count >= 5:
+                return True
+        return False
+
+    def place_stone_by_voice(self, row, col):
+        row = row - 1
+        col = col - 1
+        if self.board[row][col] is None:
+            x = col * self.cell_size + self.cell_size // 2
+            y = row * self.cell_size + self.cell_size // 2
+            color = "black" if self.current_player == "black" else "white"
+            self.canvas.create_oval(x-15, y-15, x+15, y+15, fill=color, outline="black")
+            
+            self.board[row][col] = self.current_player
+            
+            # if self.check_winner(row, col):
+            #     self.label.config(text=f"{self.current_player.capitalize()} Wins!")
+            #     self.ask_restart()
+            #     return GAME_OVER
+            
+            # self.current_player = "white" if self.current_player == "black" else "black"
+            # self.label.config(text=f"{self.current_player.capitalize()}'s Turn")
+            
+            return True
+        else:
+            return False
+
+    def reset_board(self):
+        self.canvas.delete("all")
+        self.draw_board()
+        self.board = [[None] * self.board_size for _ in range(self.board_size)]
+        self.current_player = "black"
+        self.label.config(text="Black's Turn")
+
+
+    def state_machine(self):
+        if self.state == LOADING:
+            self.label.config(text="Black's Turn")
+            self.state_label.config(text=f"Voice Recognition...")
+            self.state = VOICE_INPUT
+        
+        elif self.state == VOICE_INPUT:
+            self.row, self.col = None, None
+            self.flg = False
+            result = (self.model.listen())['text']
+            self.row, self.col = self.model.parse_position_with_correction(result)
+            
+            if self.row:
+                self.display_position(self.row, self.col)
+                self.flg = True
+                self.state = VOICE_CHECK
+            
+            else:
+                self.state_label.config(text=f"Invalid Voice. Try again...")
+                self.flg = True
+                self.state = VOICE_INPUT
+            
+        elif self.state == VOICE_CHECK:
+            self.flg = False
+            result = (self.model.listen())['text']
+            yes_or_no_or_error = self.model.yes_or_no(result)
+            
+            if yes_or_no_or_error == YES:
+                if self.place_stone_by_voice(self.row, self.col):
+                    self.flg = True
+                    self.state = GAME_CHECK
+                else:
+                    self.state_label.config(text=f"Invalid Coordinate. Try again...")
+                    self.flg = True
+                    self.state = VOICE_INPUT
+            
+            elif yes_or_no_or_error == NO:
+                self.state_label.config(text=f"Voice Recognition...")
+                self.flg = True
+                self.state = VOICE_INPUT
+            
+            elif yes_or_no_or_error == ERROR:
+                self.flg = True
+                self.state = VOICE_CHECK
+        
+        elif self.state == GAME_CHECK:
+            if self.check_winner(self.row, self.col):
+                self.label.config(text=f"{self.current_player.capitalize()} Wins")
+                self.state_label.config(text=f"Do you want to play again?")
+                self.flg = True
+                self.state = GAME_OVER
+                
+            else:
+                self.current_player = "white" if self.current_player == "black" else "black"
+                self.label.config(text=f"{self.current_player.capitalize()}'s Turn")
+                self.state_label.config(text=f"Voice Recognition...")
+                self.flg = True
+                self.state = VOICE_INPUT
+            
+        elif self.state == GAME_OVER:
+            self.flg = False
+            result = (self.model.listen())['text']
+            yes_or_no_or_error = self.model.yes_or_no(result)
+            
+            if yes_or_no_or_error == YES:
+                self.reset_board()
+                self.state_label.config(text=f"Voice Recognition...")
+                self.flg = True
+                self.state = VOICE_INPUT
+            
+            elif yes_or_no_or_error == NO:
+                self.root.quit()
+            
+            elif yes_or_no_or_error == ERROR:
+                self.flg = True
+                self.state = GAME_OVER
+        
+        self.root.after(100, self.state_machine)    
+            
+    def display_position(self, row, col):
+        row_chr = chr(row + ord('A') - 1)
+        col_chr = str(col)
+        self.state_label.config(text=(row_chr+col_chr)+" is right?")
+
+            
+if __name__ == "__main__":
+    root = tk.Tk()
+    model_path = r"vosk-model-en-us-0.22-lgraph"
+    grammar_path = r"grammar.json"
+    model = SpeechRecognizer(model_path=model_path, grammar_file=grammar_path)
+    game = Omok(root, model)
+    root.mainloop()
---
+++ speech_recognizer_model.py
@@ -0,0 +1,98 @@
+import vosk
+import sounddevice as sd
+import queue
+import json
+import threading
+import sys
+
+VOICE_INPUT = 1
+VOICE_PROCESS = 2
+GAME_OVER = 3
+EXIT = 4
+
+ERROR = 0
+NO = 1
+YES = 2
+
+class SpeechRecognizer:
+    def __init__(self, model_path, grammar_file, samplerate=16000):
+        self.model_path = model_path
+        self.grammar_file = grammar_file
+        self.samplerate = samplerate
+        self.model = vosk.Model(model_path)
+        self.recognizer = vosk.KaldiRecognizer(self.model, self.samplerate)
+        
+        with open(self.grammar_file, "r", encoding="utf-8") as f:
+            grammar_data = json.load(f)
+            self.words_list = grammar_data["words"]
+        
+        self.recognizer.SetGrammar(json.dumps(self.words_list))
+        self.audio_queue = queue.Queue()
+        self.stream = None
+
+    def callback(self, indata, frames, time, status):
+        if status:
+            print(status, file=sys.stderr)
+        self.audio_queue.put(bytes(indata))
+
+    def listen(self):
+        self.stream = sd.RawInputStream(samplerate=self.samplerate, blocksize=8000, dtype='int16',
+                                        channels=1, callback=self.callback)
+        self.stream.start()
+        
+        while True:
+            data = self.audio_queue.get()
+            if self.recognizer.AcceptWaveform(data):
+                result = json.loads(self.recognizer.Result())
+                self.stop()  # 음성 인식 완료 후 종료
+                return result
+
+    def stop(self):
+        if self.stream:
+            self.stream.stop()  # 음성 스트림 종료
+            
+    def yes_or_no(self, words):
+        words = words.lower().split()
+        if len(words) != 1:
+            return ERROR
+        
+        word = words[0]
+        
+        if word == "yes":
+            return YES
+        elif word == "no":
+            return NO
+        else:
+            return ERROR
+        
+    def word_to_number(self, word):
+        # 숫자 단어를 숫자로 매핑
+        word_map = {
+            "one": 1, "two": 2, "three": 3, "four": 4, "five": 5,
+            "six": 6, "seven": 7, "eight": 8, "nine": 9, "ten": 10,
+            "eleven": 11, "twelve": 12, "thirteen": 13, "fourteen": 14
+        }
+
+        return word_map[word]
+
+    def parse_position_with_correction(self, position):
+        words = position.lower().split()
+        if len(words) != 2:
+            return None, None
+
+        row_char = words[0]
+        col_str = words[1]
+
+        if row_char == "eight":
+            row_char = "a"
+        if col_str == "a":
+            col_str = "eight"
+
+        col = self.word_to_number(col_str)
+        
+        if not row_char.isalpha() or not col:
+            return None, None
+
+        row = ord(row_char.upper()) - ord('A') + 1
+
+        return row, col
