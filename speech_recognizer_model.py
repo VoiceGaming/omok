56805bdd16e4444838b973dac65b4c50d818cf6f
kyunsoup <rohtj_312@dgu.ac.kr>
2025-04-11 23:42:44 +0900

    TJ_up

--- speech_recognizer_model.py
+++ speech_recognizer_model.py
@@ -113,7 +113,7 @@ class SpeechRecognizer:
 
         col = self.word_to_number_omok(col_str)
         
-        if not row_char.isalpha() or not col:
+        if len(row_char)!=1 or not col or not row_char.isalpha():
             return None, None
 
         row = ord(row_char.upper()) - ord('A') + 1
